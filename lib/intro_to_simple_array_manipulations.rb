0b856ede73518d0f48ce8d9082d2aecf686294b9
Sanny Do <sanny.do94@gmail.com>
2020-08-18 00:19:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,14 @@
-def using_push(array, string)
+def using_push(Array, string)
   array.push(string)
 end
 
-def using_
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop.(array)
+end
+
+def pop_with_args(array)
+  
